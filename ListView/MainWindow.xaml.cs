415df511773d6168f7a6be414329a66dde643ea6
JRichtar <JRichtar@users.noreply.github.com>
2021-04-18 22:06:22 +0200

    Přidejte soubory projektu.

---
+++ ListView/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace List
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Person.PeopleList();
+            People.DataContext = Person.People;
+        }
+
+        private void People_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Person p = ((KeyValuePair<string, Person>)(sender as ListView).SelectedItem).Value;
+            DataContext = p;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            People.SelectedItem = 0;
+        }
+    }
+}
---
+++ ListView/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private string firstName;
+        public string FirstName
+        {
+            get => firstName;
+            set { firstName = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FirstName")); }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get => lastName;
+            set { lastName = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LastName"));}
+        }
+
+        private string birthDate;
+        public string BirthDate
+        {
+            get => birthDate;
+            set { birthDate = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BirthDate")); }
+        }
+
+        private string personIDNumber;
+        public string PersonIDNumber
+        {
+            get => personIDNumber;
+            set { personIDNumber = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PersonIDNumber")); }
+        }
+
+        public static Dictionary<string, Person> People = new Dictionary<string, Person>();
+
+        public static void PeopleList()
+        {
+            People["Jakub"] = new Person
+            {
+                FirstName = "Jakub",
+                LastName = "Richtár",
+                BirthDate = "9.9.2002",
+                PersonIDNumber = "020909/0277"
+            };
+
+            People["Bambula"] = new Person
+            {
+                FirstName = "Bambula",
+                LastName = "Kořínek",
+                BirthDate = "16.5.2015",
+                PersonIDNumber = "151605/5426"
+            };
+
+            People["Jan"] = new Person
+            {
+                FirstName = "Jan",
+                LastName = "Novák",
+                BirthDate = "1.1.2000",
+                PersonIDNumber = "000101/5188"
+            };
+        }
+    }
+}
